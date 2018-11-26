b4aeb13bfb3066b956272e56f38235c53eb65bbb
Jacob Hedrick <jacobhedrick1296@gmail.com>
2018-11-26 11:35:07 -0500

    Ref #1 Created Testing File
    Created test file and added a basic test.

---
+++ fitsTest.py
@@ -0,0 +1,15 @@
+from fits import app, db
+from utils import makeId, getHash
+import json
+
+session = db.session
+
+USERNAME = "davish20"
+FIRST = "Hananiah"
+LAST = "Davis"
+PASSWORD = "ballislife"
+
+print("################    DB TESTS   ###################")
+# Provided DB tests
+## No buckets to begin with
+assert(len(db.getUsers()) == 0)
